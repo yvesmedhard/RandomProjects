8d19ca59fe62af5e4f483fabeadb3b042f896dd2
Yves Tibe-Bi <yvesmedhard@gmail.com>
2018-03-14 03:03:01 -0300

    Atualizado pawns object Versão funcional que monta e retorna um grafo válido. Possui diversos métodos auxiliares para o grafo.

--- pawns/pawns.py
+++ pawns/pawns.py
@@ -1,79 +1,78 @@
 class Pawns:
-    def __init__():
-        self.posicloes_coluna = gerar_vetores_posicao_coluna()
-        self.config_id = preenche_config_id(gerar_vetores_posicao_coluna())
+  def __init__(self):
+    self.column_sets = self.create_column_sets()
+    self.column_set_ids = self.create_column_set_ids(self.column_sets)
+    self.tabletop_set_ids = self.create_tabletop_set_ids()
 
-    def imprime_coluna(b, p):
-      for i in range(5):
-        if p == i:
-          print("P", end="")
-        elif b == i:
-          print("B", end="")
-        else:
-          print(".", end="")      
-              
-    def enumera_configs_coluna():
-      id = 0
-      for b in range(5):
-        for p in range(5):
-          if b != p:
-            imprime_coluna(b, p)
-            print(":", id)
-            id += 1                  
+  def print_column_set(self, b, p):
+    for i in range(5):
+      if p == i:
+        print("P", end="")
+      elif b == i:
+        print("B", end="")
+      else:
+        print(".", end="")
 
-    def gerar_vetores_posicao_coluna():
-      posicoes_coluna = [list(range(20)), list(range(20))]
-      id = 0
-      for b in range(5):
-        for p in range(5):
-          if b != p:
-            posicoes_coluna[0][id] = b
-            posicoes_coluna[1][id] = p
-            id += 1
-      return posicoes_coluna
+  def print_column_sets(self):
+    id = 0
+    for b in range(5):
+      for p in range(5):
+        if b != p:
+          self.print_column_set(b, p)
+          print(":", id)
+          id += 1
 
+  def create_column_sets(self):
+    column_sets = [list(range(20)), list(range(20))]
+    id = 0
+    for b in range(5):
+      for p in range(5):
+        if b != p:
+          column_sets[0][id] = b
+          column_sets[1][id] = p
+          id += 1
+    return column_sets
 
-    def preenche_config_id(self):
-      config_id = [[-1 for i in range(5)] for j in range(5)]
-      for id in range(20):
-        config_id[self.posicoes_coluna[0][id]][self.posicoes_coluna[1][id]] = id
-      return config_id
+  def create_column_set_ids(self, column_sets):
+    column_set_ids = [[-1 for i in range(5)] for j in range(5)]
+    for id in range(20):
+      column_set_ids[column_sets[0][id]][column_sets[1][id]] = id
+    return column_set_ids
 
-    def get_id(id0, id1, id2):
-      return 400 * id2 + 20 * id1 + id0
+  def column_id(self, tabletop_id, column):
+    while column > 0:
+      tabletop_id = tabletop_id // 20
+      column = column - 1
+    return tabletop_id % 20
 
-    def get_col_id(ID, col):
-      while col > 0:
-        ID = ID // 20
-        col = col - 1
-      return ID % 20
+  def create_tabletop_set_ids(self):
+    return [[self.column_id(tabletop_id, column) for column in range(3)] for tabletop_id in range(8000)]
 
-    def col_id():
-      return [[get_col_id(ID, col) for col in range(3)] for ID in range(8000)]
+  def tabletop_set_id_from_columns_ids(self, id0, id1, id2):
+    return 400 * id2 + 20 * id1 + id0
 
-    def terminal(self, ID):
-      ganhou0 = 0
-      ganhou1 = 0
-      v = [col_id()[ID][i] for i in range(3)]
-      # jogador 0 já ganhou
-      if self.posicoes_coluna[1][3] == self.posicoes_coluna[0][v[0]] and \
-        self.posicoes_coluna[1][3] == self.posicoes_coluna[0][v[1]] and \
-        self.posicoes_coluna[1][3] == self.posicoes_coluna[0][v[2]]:
-        ganhou0 = 1
-      # jogador 1 já ganhou
-      if self.posicoes_coluna[1][v[0]] == self.posicoes_coluna[0][3] and \
-        self.posicoes_coluna[1][v[1]] == self.posicoes_coluna[0][3] and \
-        self.posicoes_coluna[1][v[2]] == self.posicoes_coluna[0][3]:
-        ganhou1 = 1
-      return ganhou0 + 2 * ganhou1
+  def tabletop_set_status(self, tabletop_id):
+    win0 = 0
+    win1 = 0
+    tabletop_columns_set = [self.tabletop_set_ids[tabletop_id][i] for i in range(3)]
+    # jogador 0 já ganhou
+    if self.column_sets[1][3] == self.column_sets[0][tabletop_columns_set[0]] and \
+      self.column_sets[1][3] == self.column_sets[0][tabletop_columns_set[1]] and \
+      self.column_sets[1][3] == self.column_sets[0][tabletop_columns_set[2]]:
+      win0 = 1
+    # jogador 1 já ganhou
+    if self.column_sets[1][tabletop_columns_set[0]] == self.column_sets[0][3] and \
+      self.column_sets[1][tabletop_columns_set[1]] == self.column_sets[0][3] and \
+      self.column_sets[1][tabletop_columns_set[2]] == self.column_sets[0][3]:
+      win1 = 1
+    return win0 + 2 * win1
 
-    def imprime_config_coluna(self, id): 
-      imprime_coluna(self.posicoes_coluna[0][id],self.posicoes_coluna[1][id])
+  def print_column_set_by_id(self, id):
+    self.print_column_set(self.column_sets[0][id],self.column_sets[1][id])
 
-    def imprime_config(ID):
-      for i in range(3):
-        id = col_id[ID][i]
-        imprime_config_coluna(id)
-        print()
+  def print_tabletop_set(self, tabletop_id):
+    for i in range(3):
+      column_id = self.tabletop_set_ids[tabletop_id][i]
+      self.print_column_set_by_id(column_id)
       print()
-Pawns().terminal(3)
+    print()
